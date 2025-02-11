a350778cc037c9ec9968ec65775cdfa6007acbeb
jonathan <jonathan.palumbo@ilife.tech>
2025-02-11 17:40:09 -0500

    Capture assume output

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,8 @@ echo "::notice:: Assuming role ($INPUT_ROLE_ARN)"
 # shellcheck disable=SC2183
 
 ASSUME_ROLE_OUTPUT="$(aws sts assume-role --role-arn "$INPUT_ROLE_ARN" --role-session-name helm-github-action-session --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" --output text)"
-echo "::notice:: ASSUME_ROLE_OUTPUT"
+echo "::notice:: $ASSUME_ROLE_OUTPUT"
+
 
 #export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
 #$(\
