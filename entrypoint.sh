785c60db447938e7c77b32e6eea3317fd3e1f3f6
jonathan <jonathan.palumbo@ilife.tech>
2025-02-11 17:30:26 -0500

    Adding some logging and testing authentication

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,24 @@
 #!/bin/ash
 
-echo "::debug::role_arn=$INPUT_ROLE_ARN"
+echo "::group::Initialization"
+echo "::notice:: input role_arn=$INPUT_ROLE_ARN"
+echo "::notice:: input eks_cluster_name=$INPUT_EKS_CLUSTER_NAME"
+echo "::notice:: input eks_cluster_name=$INPUT_EKS_CLUSTER_NAME"
 
 
+echo "::group::Authentication"
+echo "::notice:: Assuming role ($INPUT_ROLE_ARN)"
 
 # shellcheck disable=SC2046
 # shellcheck disable=SC2183
 export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
 $(aws sts assume-role \
---role-arn "$1" \
+--role-arn "$INPUT_ROLE_ARN" \
 --role-session-name MySessionName \
 --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
 --output text))
 
+echo "::notice:: Role assumed checking caller identity"
 aws sts get-caller-identity
+
+echo "::endgroup::"
